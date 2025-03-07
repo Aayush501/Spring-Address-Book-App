9d23c3544fe42cb56bf3792cafd190b0a2de8de5
Aayush501 <shrivastavashu501@gmail.com>
2025-03-07 23:45:12 +0530

    [Aayush Kumar Shrivastav] Add . Section1-UC2 -> Create a Rest Controller to demonstrate the various HTTP Methods -> Solution

---
+++ src/main/java/com/bridgelabz/addressbook/controllers/AddressBookController.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.addressbook.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class AddressBookController {
+    @GetMapping("/all")
+    public ResponseEntity<String> getAddressBook() {
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<String> getAddressByID(@PathVariable long id){
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> postAddress(@RequestBody String address){
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
+
+    @PutMapping("/put/{id}")
+    public ResponseEntity<String> putAddress(@PathVariable long id,
+                                             @RequestBody String address){
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> DeleteAddress(@PathVariable long id){
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
+}
