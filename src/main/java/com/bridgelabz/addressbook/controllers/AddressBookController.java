de8a1094d44e6b440a184f3424aba206c4c5a97b
Aayush501 <shrivastavashu501@gmail.com>
2025-03-08 00:34:08 +0530

    [Aayush Kumar Shrivastav] Add . Section3-UC2 -> Using Lombok Library for Logging

--- src/main/java/com/bridgelabz/addressbook/controllers/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbook/controllers/AddressBookController.java
@@ -2,41 +2,49 @@ package com.bridgelabz.addressbook.controllers;
 
 import com.bridgelabz.addressbook.dto.AddressDTO;
 import com.bridgelabz.addressbook.service.AddressService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/address")
 public class AddressBookController {
+
     @Autowired
     private AddressService addressService;
 
     @GetMapping("/all")
     public ResponseEntity<List<AddressDTO>> getAddressBook() {
+        log.info("Run get all element function");
         return addressService.getAddressBook();
     }
 
     @GetMapping("/get/{id}")
     public ResponseEntity<AddressDTO> getAddressByID(@PathVariable long id){
+        log.info("Run get element by id function");
         return addressService.getAddressByID(id);
     }
 
     @PostMapping("/post")
     public ResponseEntity<String> postAddress(@RequestBody AddressDTO address){
+        log.info("Run post an element function");
         return addressService.postAddress(address);
     }
 
     @PutMapping("/put/{id}")
     public ResponseEntity<String> putAddress(@PathVariable long id,
                                              @RequestBody AddressDTO address){
+        log.info("Run update element function");
         return addressService.putAddress(id, address);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> DeleteAddress(@PathVariable long id){
+        log.info("Run delete element function");
         return addressService.DeleteAddress(id);
     }
 }
--- src/main/java/com/bridgelabz/addressbook/service/AddressService.java
+++ src/main/java/com/bridgelabz/addressbook/service/AddressService.java
@@ -2,16 +2,21 @@ package com.bridgelabz.addressbook.service;
 
 import com.bridgelabz.addressbook.dto.AddressDTO;
 import com.bridgelabz.addressbook.model.AddressModel;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
+@Service
 public class AddressService {
+
     private List<AddressModel>AddressList = new ArrayList<>();
     private ModelMapper modelMapper = new ModelMapper();
 
@@ -23,6 +28,7 @@ public class AddressService {
             addressDTOList.add(addressDTO);
         }
 
+        log.info("Elements displayed");
         return new ResponseEntity<>(addressDTOList, HttpStatus.OK);
     }
 
@@ -30,15 +36,18 @@ public class AddressService {
         for(AddressModel address : AddressList){
             if(address.getId() == id){
                 AddressDTO addressDTO = modelMapper.map(address, AddressDTO.class);
+                log.info("Got the searched element");
                 return new ResponseEntity<>(addressDTO, HttpStatus.OK);
             }
         }
+        log.error("Element not found");
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     public ResponseEntity<String> postAddress(@RequestBody AddressDTO addressDTO){
         AddressModel addressModel = modelMapper.map(addressDTO, AddressModel.class);
         AddressList.add(addressModel);
+        log.info("Element added");
         return new ResponseEntity<>("Address added in book", HttpStatus.CREATED);
     }
 
@@ -47,9 +56,11 @@ public class AddressService {
             if (id == AddressList.get(i).getId()) {
                 AddressModel addressModel = modelMapper.map(addressBookDTO, AddressModel.class);
                 AddressList.set(i, addressModel);
+                log.info("Updated the element");
                 return new ResponseEntity<>("Updated", HttpStatus.OK);
             }
         }
+        log.error("Element not found.");
         return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
     }
 
@@ -57,9 +68,12 @@ public class AddressService {
         for (int i = 0; i < AddressList.size(); i++) {
             if (id == AddressList.get(i).getId()) {
                 AddressList.remove(i);
+                log.info("Element has been deleted");
                 return new ResponseEntity<>("Deleted", HttpStatus.OK);
             }
         }
+        log.error("No such element exist");
         return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
     }
+
 }
